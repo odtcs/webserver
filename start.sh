bb4020c4403f462cf6f7646882230fe779cec0b2
Jasper Boot <J4sp3r@users.noreply.github.com>
2019-03-05 18:51:23 +0100

    Fixes cloning issue on production

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 
 ######## SCREEN SOFTWARE ########
 rm -r ./screen
-git clone git@github.com:odtcs/screen.git
+git clone https://github.com/odtcs/screen.git
 ######## END OF SCREEN SOFTWARE ########
 
 ######## WORKSHOP HACK ########
 rm -r ./workshop-hack
-git clone git@github.com:odtcs/workshop-hack.git
+git clone https://github.com/odtcs/workshop-hack.git
 ######## END OF WORKSHOP HACK ########
 
 
